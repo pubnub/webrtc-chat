77f52aed22fc523012abed98ffbba7541564b0b4
dristic <dristic101@gmail.com>
2013-11-15 15:34:02 -0800

    New build of page.

--- Gruntfile.js
+++ Gruntfile.js
@@ -299,7 +299,8 @@ module.exports = function (grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*',
+                        'CNAME'
                     ]
                 }]
             },
